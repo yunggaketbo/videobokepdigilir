c6978ce1d92e13240434871467e8dfb8a015a134
yunggaketbo <yunggaketbo@gmail.com>
2025-06-30 14:58:19 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Video Bokep Digilir';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil jilbab smp tante.';
+export const desk = 'Video Bokep Indo Jepang Jav Hd Gilir Viral Terbaru Bocil Ngentot Jilbab Smp Montok';
 export const url = 'https://videobokepdigilir.pages.dev';
-export const terbit = '2025-06-26T00:00:00Z';
+export const terbit = '2025-06-26T00:01:03Z';
